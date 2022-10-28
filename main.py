1c84ddcfc2f8d4ed4e2b035baea56d70b3023e7d
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-10-28 11:51:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ import json
 import subprocess
 
 url = 'http://ip-api.com/json'
-
 p1 = requests.get(url)
 data = json.loads(p1._content)
-
 print("\nINFORMAÇÃO DA CONEXÃO\nDEV: Cristiano Ferreira\n\n")
 print('IP PÚBLICO..: {}\nPAÍS........: {}\nREGIÃO......: {}\nPROVEDOR....: {}\n'.format(data["query"], data["country"], data["regionName"], data["isp"]))
 
 result = subprocess.run("ip route> log.txt", shell=True)
 
+def default():
+  print("\nIP PRIVADO..: {}".format(rx[13]))
+  print("REDE LOCAL..: {}".format(rx[5]))
+  print("INTERFACE...: {}".format(rx[4]),"\n")
+
+def mobile():
+  print("\nIP PRIVADO..: {}".format(rx[8]))
+  print("REDE LOCAL..: {}".format(rx[0]))
+  print("INTERFACE...: {}".format(rx[2]),"\n")
+
 f = open("log.txt", "r")
 dat = f.read()
 rx = dat.split()
+retorno = len(rx)
 
-print("IP PRIVADO..: {}".format(rx[13]))
-print("REDE LOCAL..: {}".format(rx[5]))
-print("INTERFACE...: {}".format(rx[4]))
+if retorno > 9:
+  default()
+else:
+  mobile()
