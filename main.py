06c902344d731a1af9d24fbb6b450bb2c705a5d8
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-10-28 10:50:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ url = 'http://ip-api.com/json'
 
 p1 = requests.get(url)
 data = json.loads(p1._content)
-print("\nINFORMAÇÃO DA CONEXÃO - DEV: Cristiano Ferreira\n\n")
-print('IP..........: {}\nPAÍS........: {}\nREGIÃO......: {}\nPROVEDOR....: {}\n'.format(data["query"], data["country"], data["city"], data["isp"]))
 
-result = subprocess.run("ifconfig eth0 > log.txt", shell=True)
+print("\nINFORMAÇÃO DA CONEXÃO\nDEV: Cristiano Ferreira\n\n")
+print('IP PÚBLICO..: {}\nPAÍS........: {}\nREGIÃO......: {}\nPROVEDOR....: {}\n'.format(data["query"], data["country"], data["regionName"], data["isp"]))
 
-with open(r"log.txt", 'r') as fp:
-    line_numbers = [1]
-    lines = []
-    for i, line in enumerate(fp):
-        if i in line_numbers:
-            lines.append(line.strip())
-        elif i > 1:
-            break
-print('CONEXÃO LOCAL:\n'+lines[0]+'\n')
+result = subprocess.run("ip route> log.txt", shell=True)
+
+f = open("log.txt", "r")
+dat = f.read()
+rx = dat.split()
+
+print("IP PRIVADO..: {}".format(rx[13]))
+print("REDE LOCAL..: {}".format(rx[5]))
+print("INTERFACE...: {}".format(rx[4]))
