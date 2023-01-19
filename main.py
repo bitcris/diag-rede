682042d9f65f8461728d6f504a876b46fd47f7c2
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2023-01-19 16:11:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
+import dns.resolver
 import requests
 import json
 import subprocess
 
-url = 'http://ip-api.com/json'
-p1 = requests.get(url)
-data = json.loads(p1._content)
-print("\nINFORMAÇÃO DA CONEXÃO\nDEV: Cristiano Ferreira\n\n")
-print('IP PÚBLICO..: {}\nPAÍS........: {}\nREGIÃO......: {}\nPROVEDOR....: {}\n'.format(data["query"], data["country"], data["regionName"], data["isp"]))
+
+dns_resolver = dns.resolver.Resolver()
+dns = dns_resolver.nameservers[0]
+
+
+
+api = 'http://ip-api.com/json/'
+apiDns = api+dns
+
+reqDns = requests.get(apiDns)
+reqIp = requests.get(api)
+dataDns = json.loads(reqDns.text)
+dataIp = json.loads(reqIp.text)
 
 result = subprocess.run("ip route> log.txt", shell=True)
-
-def default():
-  print("\nIP PRIVADO..: {}".format(rx[13]))
-  print("REDE LOCAL..: {}".format(rx[5]))
-  print("INTERFACE...: {}".format(rx[4]),"\n")
-
-def mobile():
-  print("\nIP PRIVADO..: {}".format(rx[8]))
-  print("REDE LOCAL..: {}".format(rx[0]))
-  print("INTERFACE...: {}".format(rx[2]),"\n")
-
 f = open("log.txt", "r")
 dat = f.read()
 rx = dat.split()
 retorno = len(rx)
 
-if retorno > 9:
-  default()
-else:
-  mobile()
+print("\n\n-------- DADOS DA REDE -------\n")
+print('IP PÚBLICO: {}'.format(dataIp["query"]))
+print('PROVEDOR: {}'.format(dataIp["isp"]))
+print('PAÍS: {}\nREGIÃO: {}\n\n'.format(dataIp["country"], dataIp["regionName"]))
+
+print("SERVIDOR DNS")
+print('IP: {}\n\n'.format(dns))
+print('SERVIDOR: {}\n\n'.format(dataDns["isp"]))
+
+print("REDE LOCAL")
+print(rx)
